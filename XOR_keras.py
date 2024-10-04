7c26d7f4c3bdce74db8dfec9141422486a98fe83
Philip Sykes <philipsykes2004@icloud.com>
2024-10-04 13:52:25 +0100

    created neural network with disabled hidden layer

--- XOR_keras.py
+++ XOR_keras.py
@@ -1,2 +1,14 @@
+import numpy as np
 import keras
+from keras import layers
 
+input_data = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
+output_data = np.array([[0], [1], [1], [0]])
+
+
+model = keras.Sequential()
+model.add(layers.Dense(2, input_dim=2, activation='relu')) # 2 neurons in input layer
+#model.add(layers.Dense(2, activation='tanh'))
+model.add(layers.Dense(1, activation='sigmoid'))
+
+model.summary()
